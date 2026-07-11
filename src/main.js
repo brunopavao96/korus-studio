3da0feb14262203299ab83efa38eba94fbc061f3
brunopavao96 <brunopavao1612@gmail.com>
2026-07-11 18:58:40 -0300

    feat: render track list dynamically

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
-const songPath = "songs/somebody-told-me/song.json";
+const SONG_PATH = "songs/somebody-told-me/song.json";
 
-async function loadSong() {
-    const response = await fetch(songPath);
+const songTitleElement = document.querySelector('#song-title');
+const songArtistElement = document.querySelector('#song-artist');
+const trackContainer = document.querySelector('#tracks-container');
+
+
+
+async function songLoad(){
+    const response = await fetch(SONG_PATH);
     const song = await response.json();
-    console.log(song);
+    renderSong(song);
+};
+
+function renderSong (song){
+    songTitleElement.textContent = song.title;
+    songArtistElement.textContent = song.artist;
+    song.tracks.forEach(track => {
+        const trackElement = document.createElement('div');
+        trackElement.textContent = track.name;
+        trackContainer.appendChild(trackElement)
+    });
 }
-loadSong();
+songLoad()
