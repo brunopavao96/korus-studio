62579b6350075d8a6216f9f9ac1567d64b1c94b7
brunopavao96 <brunopavao1612@gmail.com>
2026-07-11 11:56:40 -0300

    feat: load song metadata from JSON

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
 const songPath = "songs/somebody-told-me/song.json";
+
+async function loadSong() {
+    const response = await fetch(songPath);
+    const song = await response.json();
+    console.log(song);
+}
+loadSong();
