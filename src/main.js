55b5565e7bd9c0b329cc7ca6843a2a55f9e34725
brunopavao96 <brunopavao1612@gmail.com>
2026-07-13 18:15:52 -0400

    feat: primeira versão funcional do Korus Studio (v0.1.0)

--- src/main.js
+++ src/main.js
@@ -2,25 +2,37 @@ const SONG_FOLDER = "songs/somebody-told-me";
 const SONG_PATH = `${SONG_FOLDER}/song.json`;
 const audioTracks = [];
 
+let atualTrack;
+let progressTrack;
+let durationTrack
+
 const songTitleElement = document.querySelector('#song-title');
 const songArtistElement = document.querySelector('#song-artist');
 const trackContainer = document.querySelector('#tracks-container');
 const playerControls = document.querySelector("#player-controls");
+const sliderProgress = document.querySelector("#slider-progress");
+const containerLyrics = document.querySelector('.container-lyrics');
 
 
 
 async function songLoad(){
     const response = await fetch(SONG_PATH);
     const song = await response.json();
-    
-
-    renderSong(song);
+    await renderSong(song);
 };
 
-function renderSong (song){
+async function renderLyrics(song){
+    const response = await fetch(`${SONG_FOLDER}/${song.lyrics}`);
+    const lyrics = await response.text();
+    containerLyrics.textContent = lyrics;
+}
+
+async function renderSong (song){
     renderHeader(song);
-    renderTracks(song);
     renderControls();
+    renderProgress();
+    renderTracks(song);    
+    await renderLyrics(song);
 }
 
 function renderHeader(song){
@@ -36,11 +48,27 @@ function renderTracks(song){
         const trackAudio = document.createElement('audio');
         trackAudio.src = `${SONG_FOLDER}/tracks/${track.file}`;
         trackAudio.preload = "auto";
+        if (audioTracks.length === 0) {
+
+        trackAudio.addEventListener("loadedmetadata", () => {
+            durationTrack.textContent = formatTime(trackAudio.duration);
+        });
+
+        trackAudio.addEventListener("timeupdate", () => {
+            atualTrack.textContent = formatTime(trackAudio.currentTime);
+            progressTrack.value = (trackAudio.currentTime / trackAudio.duration) * 100;
+        });
+
+    }
+        
         audioTracks.push(trackAudio);
         trackSlider.type = "range";
         trackSlider.min = 0;
         trackSlider.max = 100;
         trackSlider.value = 100;
+        trackSlider.addEventListener('input', () => {
+            trackAudio.volume = trackSlider.value / 100;
+        });
         trackName.textContent = track.name;
         trackElement.appendChild(trackName);
         trackElement.appendChild(trackSlider);
@@ -56,8 +84,8 @@ function renderControls (){
     pauseButton.textContent = 'Pause';
     stopButton.textContent = 'Stop';
     playButton.addEventListener('click', playTracks);
-    //pauseButton.addEventListener('click', pauseTrack);
-    //stopButton.addEventListener('click', stopTracks);
+    pauseButton.addEventListener('click', pauseTracks);
+    stopButton.addEventListener('click', stopTracks);
     playerControls.appendChild(playButton);
     playerControls.appendChild(pauseButton);
     playerControls.appendChild(stopButton);
@@ -65,8 +93,58 @@ function renderControls (){
 
 function playTracks(){
     audioTracks.forEach( audio => {
+        
         audio.play();
     })
 };
 
+function pauseTracks(){
+    audioTracks.forEach( audio => {
+        audio.pause();
+    })
+}
+
+function stopTracks(){
+    audioTracks.forEach( audio => {
+        audio.pause();
+        audio.currentTime = 0;
+    })
+}
+
+function renderProgress(){
+    atualTrack = document.createElement('span');
+    progressTrack = document.createElement('input'); 
+    durationTrack = document.createElement('span');
+    progressTrack.type = "range";
+    progressTrack.max = 100;
+    progressTrack.min = 0;
+    progressTrack.value = 0;
+    atualTrack.textContent = "00:00";
+    durationTrack.textContent = "00:00";
+    progressTrack.addEventListener('input', () => {
+        audioTracks.forEach(audio => {
+            audio.currentTime = (progressTrack.value / 100) * audio.duration
+        })
+    })
+    sliderProgress.appendChild(atualTrack);
+    sliderProgress.appendChild(progressTrack);
+    sliderProgress.appendChild(durationTrack);
+}
+
+function formatTime (seconds){
+
+    let minutes = Math.floor(seconds / 60)
+    let secondsLeft = Math.floor(seconds % 60);
+    
+    if (secondsLeft < 10){
+        secondsLeft = "0" + secondsLeft;
+    };
+
+    if (minutes < 10){
+        minutes = "0" + minutes;
+    }
+
+    return `${minutes}:${secondsLeft}`
+    
+}
 songLoad()
